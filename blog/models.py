d8e943b6aeef07b490c518cf4367aacd2f1db560
ysm <ysm@tourbell.co.kr>
2019-04-25 17:35:12 +0900

    - 모델 verbose 네이밍 추가

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from common.util.utils import user_path
-# Create your models here.
 
 
 class bloglist(models.Model):
-    Title = models.CharField(max_length=30000)
-    SubTitle = models.CharField(max_length=200)
-    Subject = models.TextField(default='')
-    picture = models.ImageField(upload_to=user_path, blank=True)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    Title = models.CharField(max_length=30000, verbose_name="제목")
+    SubTitle = models.CharField(max_length=200, verbose_name="부제목")
+    Subject = models.TextField(default='', verbose_name="내용")
+    picture = models.ImageField(upload_to=user_path, blank=True, verbose_name="배경사진")
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name="추가날짜")
